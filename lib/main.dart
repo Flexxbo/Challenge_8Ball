56069947622f68afecf00ddc519c957300177ca9
Flexxbo <flexxbo@gmail.com>
2021-02-18 00:50:21 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      EightBall(),
+    );
+
+class EightBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
